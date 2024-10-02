ae52d781e89b66d26a93f0106befbf1687389df7
kushagra agrawal <agrawalkush2020@gmail.com>
2024-10-02 17:09:54 +0530

    timing added

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import City, Location, Movie
+from .models import City, Location, Movie, Timing
 
 admin.site.register(City)
 admin.site.register(Location)
 admin.site.register(Movie)
+admin.site.register(Timing)
