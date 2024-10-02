0c9dffd38fce78fee426ea98600720b09194f1e6
kushagra agrawal <agrawalkush2020@gmail.com>
2024-10-02 12:31:01 +0530

    model corrected

--- movies/models.py
+++ movies/models.py
@@ -6,26 +6,29 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+
 class Location(models.Model):
     address = models.CharField(max_length=255)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='locations')
 
     def __str__(self):
         return f'{self.address}, {self.city.name}'
 
+
+class Timing(models.Model):
+    start_time = models.TimeField()  # start time of the movie
+    end_time = models.TimeField()    # end time of the movie
+
+    def __str__(self):
+        return f'Start: {self.start_time}, End: {self.end_time}'
+
+
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    cities = models.ManyToManyField(City, related_name='movies')
-    locations = models.ManyToManyField(Location, related_name='locations')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    number_of_seats = models.PositiveIntegerField()
+    name = models.CharField(max_length=100)  #done
+    cities = models.ManyToManyField(City, related_name='movies')    #done
+    locations = models.ManyToManyField(Location, related_name='locations')    #done
+    price = models.DecimalField(max_digits=10, decimal_places=2)    #done
+    timings = models.ManyToManyField(Timing, related_name='timings')   #done
 
     def __str__(self):
         return self.name
 
-class Timing(models.Model):
-    time = models.TimeField()  # Field to store the timing of the movie
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='timings')  # Link to the Movie model
-
-    def __str__(self):
-        return f'{self.movie.name} at {self.time}'
