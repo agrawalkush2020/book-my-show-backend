10377886d7cfb43b5dbceff18e9a830b328bcbfc
kushagra agrawal <agrawalkush2020@gmail.com>
2024-10-08 00:32:11 +0530

    require updated

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-import sys
+import argcomplete, sys
+from django.core.management import execute_from_command_line
 
+argcomplete.autocomplete(sys.argv)
 
 def main():
     """Run administrative tasks."""
