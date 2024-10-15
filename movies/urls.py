a1817c815e62e7c9210354ec348c9c38297cab27
kushagra agrawal <agrawalkush2020@gmail.com>
2024-10-16 01:28:37 +0530

    have to make new api for locations

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from movies import views
 urlpatterns = [
     # path('get_all_locations/', views.get_all_locations),/
     path('get_all_movies/', views.get_all_movies),
+    path('get_all_locations_and_timings/', views.get_all_locations_and_timings),
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from .models import City, Cinema, Show
 from django.http import HttpResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def get_all_movies(request):
     params = json.loads(request.body)
-
-    city_name = params.get("city", "Gurugram")
+    city_name = params.get("city", "Mumbai")
     city = City.objects.get(name=city_name)
 
     cinemas = Cinema.objects.filter(city=city)
@@ -20,14 +21,24 @@ def get_all_movies(request):
             movies_set.add(show.movie)  # Add movie to the set
 
     # Convert the set back to a list (if needed)
-    movies = list(movies_set)
-    for movie in movies:
-
-
-
+    movies = []
+    movies_list = list(movies_set)
+    for movie in movies_list:
+        movies.append({"id": movie.id, "name": movie.name, "rating": movie.rating})
 
     return HttpResponse(json.dumps({"success": True, "movies": movies}), content_type="application/json",
                         status=200)
+
+
+@csrf_exempt
+def get_all_locations_and_timings(request):
+    params = json.loads(request.body)
+    movie_id = params.get('movie_id')
+    city = params.get('city')
+
+
+
+
 
 
 
