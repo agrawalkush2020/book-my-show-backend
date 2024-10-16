683d8376b4a227de0c24696a2731e98314809b79
kushagra agrawal <agrawalkush2020@gmail.com>
2024-10-17 00:35:06 +0530

    all details for one movie added

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import City, Cinema, Show
+from .models import City, Cinema, Show, Movie
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -36,10 +36,24 @@ def get_all_locations_and_timings(request):
     movie_id = params.get('movie_id')
     city = params.get('city')
 
+    try:
+        movie_object = Movie.objects.get(id=movie_id)
+        city_object = City.objects.get(name=city)
 
+        # Get all cinema objects in the given city that are showing the particular movie
+        cinemas = Cinema.objects.filter(city=city_object, show__movie=movie_object).distinct()
 
+        locations = [{"service_provider": "INOX", "mall": "Huda city Center", "city": "Gurugram",
+                      "timings": ["10:00 AM", "09:30 PM"]}]
+        for cinema in cinemas:
+            # Access the service provider associated with the cinema
+            service_provider = cinema.service_provider
+            locations.append({"service_provider": service_provider.name, "mall": cinema.mall, "city": cinema.city,
+                              "timings": []})
 
+        return HttpResponse(json.dumps({"success": True, "locations": locations}), content_type="application/json",
+                            status=200)
 
-
-
-
+    except Exception as ex:
+        return HttpResponse(json.dumps({"success": False, "message": ex}), content_type="application/json",
+                            status=500)
